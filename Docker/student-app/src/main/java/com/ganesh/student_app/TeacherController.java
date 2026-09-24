818b0454396c56971f964b15f4cabe5d72a4737d
GaneshPawar26 <ganeshnpawar2003@gmail.com>
2026-09-24 22:14:30 +0530

    B commit

---
+++ Docker/student-app/src/main/java/com/ganesh/student_app/TeacherController.java
@@ -0,0 +1,12 @@
+package com.ganesh.student_app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeacherController {
+
+    public String hello()
+    {
+        return "cnskcjhjc";
+    }
+}
