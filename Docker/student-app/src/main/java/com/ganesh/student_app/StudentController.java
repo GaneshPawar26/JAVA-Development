d566f38481e58f935d4d4ff5042586cf1d7e7f45
GaneshPawar26 <ganeshnpawar2003@gmail.com>
2026-09-24 16:15:24 +0530

    A commit

--- Docker/student-app/src/main/java/com/ganesh/student_app/StudentController.java
+++ Docker/student-app/src/main/java/com/ganesh/student_app/StudentController.java
@@ -1,4 +1,16 @@
 package com.ganesh.student_app;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+
+
+    @GetMapping("/")
+    public String hello()
+    {
+     return "hello student";
+    }
 }
